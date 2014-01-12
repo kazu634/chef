ec44378d15d249095ecb880c9ac39d6ae848687d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-01-12 14:36:21 +0900

    Deploy the `monit` configuration file for redis.
    This commit refs #6.

--- site-cookbooks/sensu-custom/recipes/server_settings.rb
+++ site-cookbooks/sensu-custom/recipes/server_settings.rb
@@ -14,4 +14,14 @@ include_recipe "sensu-custom::server_handlers"
 include_recipe "iptables"
 iptables_rule  "rabbitmq"
 
+cookbook_file "/etc/monit/conf.d/redis.conf" do
+  source "redis.conf"
+
+  owner "root"
+  group "root"
+  mode 0644
+
+  notifies :restart,  "service[monit]"
+end
+
 include_recipe "sensu-custom::server_varnish"
