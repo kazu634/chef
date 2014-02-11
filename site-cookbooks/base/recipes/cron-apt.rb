8c57ead712638e52d0b2dd999112f8ee3f11d1f6
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-02-11 14:33:41 +0900

    Install `cron-apt` package.
    This commit refs #15.

---
+++ site-cookbooks/base/recipes/cron-apt.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: base
+# Recipe:: cron-apt
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "cron-apt" do
+  action :install
+end
