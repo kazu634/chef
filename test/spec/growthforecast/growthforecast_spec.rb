d7e3b498d670424518f139de17cbc2adb125236e
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-04-27 22:36:25 +0900

    Add test for nginx configuration file.
    This commit refs #71.

--- test/spec/growthforecast/growthforecast_spec.rb
+++ test/spec/growthforecast/growthforecast_spec.rb
@@ -33,3 +33,19 @@ describe service('growthforecast') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file('/etc/nginx/sites-available/growth') do
+  it { should be_file }
+
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+
+  it { should be_mode 644 }
+
+  it { should match_md5checksum '163657859bd82223b8f7f5827b0b947a' }
+end
+
+describe file('/etc/nginx/sites-enabled/growthforecast') do
+  it { should be_linked_to '/etc/nginx/sites-available/growth' }
+end
+
