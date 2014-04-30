a4edc9998d231704e0a5890de4993cb23ca3f61e
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-04-30 17:46:57 +0900

    Add test for making sure that the configuration directory is made.
    This commit refs #78.

--- test/spec/fluentd-custom/td-agent_spec.rb
+++ test/spec/fluentd-custom/td-agent_spec.rb
@@ -8,3 +8,12 @@ describe service('td-agent') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe file('/etc/td-agent/conf.d') do
+  it { should be_directory }
+
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+
+  it { should be_mode 755 }
+end
