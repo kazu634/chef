0ae196882c1348143a3be8a66297d4aa7a9ca660
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-06-11 23:26:26 +0900

    Modify `metadata.rb` to pass the `foodcritic`'s criteria.
    This commit refs #79.

--- site-cookbooks/webapp/metadata.rb
+++ site-cookbooks/webapp/metadata.rb
@@ -1,7 +1,10 @@
 name             'webapp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Kazuhiro MUSASHI'
+maintainer_email 'simoom634@yahoo.co.jp'
 license          'All rights reserved'
 description      'Installs/Configures webapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "nginx"
+depends "ruby"
