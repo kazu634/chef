71bb904f5470148399c81bfe7bc190c4e5c710a3
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-10-04 22:07:46 +0900

    Deploy the configuration file for hipchat notification.
    - Deploy the configuration file for hipchat notification.
    - Add encrypted data bag for hipchat notification.
    
    This commit refs #112.

--- site-cookbooks/fluentd-custom/recipes/hipchat.rb
+++ site-cookbooks/fluentd-custom/recipes/hipchat.rb
@@ -16,4 +16,22 @@ if node[:td_agent][:forward]
       gem_binary '/usr/lib/fluent/ruby/bin/fluent-gem'
     end
   end
+
+  hipchat_auth = Chef::EncryptedDataBagItem.load("fluentd-custom",  "hipchat")
+
+  # deploy the configuration file for hipchat notification
+  template "/etc/td-agent/conf.d/watcher.conf" do
+    source "watcher.erb"
+
+    owner "root"
+    group "root"
+
+    mode 0644
+
+    variables({
+      :api_token => hipchat_auth["api_token"]
+    })
+
+    notifies :restart,  "service[td-agent]"
+  end
 end
