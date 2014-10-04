36cacbfcdc599346484c944e4c45c22794ce1f09
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-10-04 21:24:16 +0900

    Deploy the configuration file for monitoring /var/log/apt/history.log.
    This commit refs #30.

--- site-cookbooks/fluentd-custom/recipes/aptitude.rb
+++ site-cookbooks/fluentd-custom/recipes/aptitude.rb
@@ -17,3 +17,16 @@
       gem_binary '/usr/lib/fluent/ruby/bin/fluent-gem'
   end
 end
+
+# deploy the configuration file for monitoring /var/log/apt/history.log
+cookbook_file "/etc/td-agent/conf.d/forwarder_aptitude.conf" do
+  source "forwarder_aptitude.conf"
+
+  owner "root"
+  group "root"
+
+  mode 0644
+
+  notifies :restart,  "service[td-agent]"
+end
+
