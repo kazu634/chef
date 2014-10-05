7c2266d681757d4ea15c206580b69d93ade8091a
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-10-05 23:25:32 +0900

    Deploy the configuration file for processing nginx logs.
    This commit refs #73.

--- site-cookbooks/fluentd-custom/recipes/nginx.rb
+++ site-cookbooks/fluentd-custom/recipes/nginx.rb
@@ -34,3 +34,18 @@ cookbook_file "/etc/td-agent/conf.d/forwarder_nginx.conf" do
 
   notifies :restart, "service[td-agent]"
 end
+
+# deploy the configuration file for processing nginx logs
+cookbook_file "/etc/td-agent/conf.d/processor_nginx.conf" do
+  source "processor_nginx.conf"
+
+  owner "root"
+  group "root"
+
+  mode 0644
+
+  # if the node is the fluentd manager:
+  only_if { node[:td_agent][:forward] }
+
+  notifies :restart, "service[td-agent]"
+end
