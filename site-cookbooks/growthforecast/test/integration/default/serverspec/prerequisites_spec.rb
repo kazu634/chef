de7dc9019fede46f7e2e8417a7a834e42e0e7d07
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-11-03 21:10:22 +0900

    [Growthforecast] Support `test-kitchen`
    - Support `test-kitchen` environment.

---
+++ site-cookbooks/growthforecast/test/integration/default/serverspec/prerequisites_spec.rb
@@ -0,0 +1,17 @@
+require 'serverspec'
+
+set :backend,  :exec
+
+describe user('growth') do
+  it { should exist }
+  it { should have_home_directory '/var/lib/growthforecast' }
+  it { should have_login_shell '/bin/bash' }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe package('fonts-ipafont') do
+  it { should be_installed }
+end
