03b3aed26eac8d98d53412995de2d4647696dac7
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-11-09 10:42:18 +0900

    Remove unnecessary AWS configurations.
    Do *not* install the AWS-related tools that no longer are used.

--- site-cookbooks/kazu634/recipes/ruby.rb
+++ site-cookbooks/kazu634/recipes/ruby.rb
@@ -21,31 +21,6 @@ directory "/home/kazu634/bin" do
   action   :create
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/ec2-api-tools.zip" do
-  source "http://www.amazon.com/gp/redirect.html/ref=aws_rc_ec2tools?location=http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip&token=A80325AA4DAB186C80828ED5138633E3F49160D9"
-
-  mode 0644
-
-  not_if "test -e /home/kazu634/bin/ec2-api-tools"
-end
-
-script "Install ec2 api tools" do
-  interpreter "bash"
-
-  user "root"
-  group "root"
-
-  cwd "#{Chef::Config[:file_cache_path]}/"
-
-  code <<-EOH
-    unzip -o ec2-api-tools.zip
-    chown -R kazu634:kazu634 ec2-api-tools*
-    find `pwd` -maxdepth 1 -type d -name "ec2-api-tools*" | xargs -I % mv % /home/kazu634/bin/ec2-api-tools
-  EOH
-
-  not_if "test -e /home/kazu634/bin/ec2-api-tools"
-end
-
 directory "/home/kazu634/.chef" do
   owner    "kazu634"
   group    "kazu634"
@@ -56,23 +31,6 @@ end
 
 template "/home/kazu634/.chef/knife.rb" do
   source "knife.rb.erb"
-
-  owner "kazu634"
-  group "kazu634"
-
-  mode 0644
-end
-
-directory "/home/kazu634/works/knife-ec2" do
-  owner    "kazu634"
-  group    "kazu634"
-  mode     0775
-
-  action   :create
-end
-
-template "/home/kazu634/works/knife-ec2/template.erb" do
-  source "template.erb"
 
   owner "kazu634"
   group "kazu634"
