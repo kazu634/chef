e22e6365b99a6d3a8eb952965a952753dfc700d1
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-11-09 23:12:08 +0900

    [monit] Support `test-kitchen`
    - Support `test-kitchen`
    - Add tests

---
+++ site-cookbooks/monit/test/integration/default/serverspec/monit_spec.rb
@@ -0,0 +1,23 @@
+require 'serverspec'
+
+set :backend,  :exec
+
+describe package('monit') do
+  it { should be_installed }
+end
+
+describe file('/etc/monit/monitrc') do
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 600 }
+
+  its(:md5sum) { should eq '3b82c533d8471c12246e9b2115a2c429' }
+end
+
+describe file('/etc/init/monit.conf') do
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 644 }
+
+  its(:md5sum) { should eq 'b8ac9f6d996a2ebf75ada70e8a8f2ccb' }
+end
