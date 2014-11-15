d9f8a3028d176c53e743b3533e9ff28772f24fe5
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-11-15 15:02:21 +0900

    Add the following prerequisite packages for installing `Ruby`:
    - libreadline6
    - libreadline6-dev

--- site-cookbooks/ruby/recipes/default.rb
+++ site-cookbooks/ruby/recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe "apt"
 include_recipe "build-essential"
 include_recipe "base"
 
-pre_requisites = %w{libreadline-dev libssl-dev zlib1g-dev libssl1.0.0 libxml2-dev libxslt-dev}
+pre_requisites = %w{libreadline-dev libssl-dev zlib1g-dev libssl1.0.0 libxml2-dev libxslt-dev libreadline6 libreadline6-dev }
 
 pre_requisites.each do |p|
   package p do
