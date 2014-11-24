3b84d5f9009b3568f6276c566fe52e4baf333f4c
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-11-24 10:27:30 +0900

    [jenkins] Support `environment`

--- site-cookbooks/jenkins/attributes/default.rb
+++ site-cookbooks/jenkins/attributes/default.rb
@@ -1,2 +1,7 @@
-default["jenkins"]["hostname"]="jenkins"
-default["jenkins"]["domain"]="kazu634.com"
+# common configuration
+default["jenkins"]["hostname"]     = "jenkins"
+
+# domain name
+default_unless["common"]["domain"] = 'com'
+
+default["jenkins"]["domain"]       = "kazu634.#{node['common']['domain']}"
