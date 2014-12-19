03c05e806c03272591fe4619267bb1f3075beb5d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-12-19 23:35:01 +0900

    Add default value for installing `peco`.

---
+++ site-cookbooks/peco/attributes/default.rb
@@ -0,0 +1,5 @@
+# default values:
+
+default['peco']['version'] = '0.2.11'
+default['peco']['url']     = "https://github.com/peco/peco/releases/download/v#{node['peco']['version']}/peco_linux_amd64.tar.gz"
+default['peco']['prefix']  = '/usr/local'
