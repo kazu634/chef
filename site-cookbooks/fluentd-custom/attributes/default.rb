86f24879dd8c457e2ac64faa2033f0e6284c3c5b
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-01-09 22:35:55 +0900

    [fluentd-custom] Do not configure `iptables` settings in testing environment.

--- site-cookbooks/fluentd-custom/attributes/default.rb
+++ site-cookbooks/fluentd-custom/attributes/default.rb
@@ -3,6 +3,7 @@ default['td_agent']['api_key']      = 'foo'
 default['td_agent']['version']      = '2'
 default['td_agent']['forward']      = false
 default['td_agent']['base']         = true
+default['td_agent']['ssh']          = true
 
 # domain configuration
 default_unless['common']['domain']  = 'com'
