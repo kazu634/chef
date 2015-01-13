78e60583ca910b026d044ad170c6a105e6e80c16
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-01-13 21:35:45 +0900

    [notification] Follow the `rubocop` comment and modify the code.
    ```
    --- a/site-cookbooks/notification/metadata.rb
    +++ b/site-cookbooks/notification/metadata.rb
    @@ -1,7 +1,7 @@
    -name             'notification'
    -maintainer       'Kazuhiro MUSASHI'
    +name 'notification'
    +maintainer 'Kazuhiro MUSASHI'
     maintainer_email 'simoom634@yahoo.co.jp'
    -license          'All rights reserved'
    -description      'Installs/Configures notification'
    +license 'All rights reserved'
    +description 'Installs/Configures notification'
     long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
    -version          '0.1.0'
    +version '0.1.0'
    ```

--- site-cookbooks/notification/metadata.rb
+++ site-cookbooks/notification/metadata.rb
@@ -1,7 +1,7 @@
-name             'notification'
-maintainer       'Kazuhiro MUSASHI'
+name 'notification'
+maintainer 'Kazuhiro MUSASHI'
 maintainer_email 'simoom634@yahoo.co.jp'
-license          'All rights reserved'
-description      'Installs/Configures notification'
+license 'All rights reserved'
+description 'Installs/Configures notification'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version '0.1.0'
