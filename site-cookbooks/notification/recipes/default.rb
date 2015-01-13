ada1438140e73faaceb3933b02686565bf65a91d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-01-13 20:24:43 +0900

    [notification] Enable `Slack` notification.

--- site-cookbooks/notification/recipes/default.rb
+++ site-cookbooks/notification/recipes/default.rb
@@ -6,3 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+chef_gem 'chef-handler-slack' do
+  action :upgrade
+end
+
+require 'chef/handler/slack'
+
+slack = Chef::EncryptedDataBagItem.load('notification', 'slack')
+
+chef_handler 'Chef::Handler::SlackReporting' do
+  source 'chef/handler/slack'
+  arguments [
+    # The name of your team registered with Slack
+    team: 'kazu634',
+
+    # Your incoming webhook token
+    token: slack['token'],
+
+    # An existing channel
+    channel: '#chef',
+
+    # Watever.
+    icon_emoj: ':chef:'
+  ]
+  action :nothing
+end.run_action(:enable)
