144ad46900ee58971769271bf5c1437b6c0b60ae
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-01-13 20:17:14 +0900

    [notification] Create `notification` cookbook.

---
+++ site-cookbooks/notification/metadata.rb
@@ -0,0 +1,7 @@
+name             'notification'
+maintainer       'Kazuhiro MUSASHI'
+maintainer_email 'simoom634@yahoo.co.jp'
+license          'All rights reserved'
+description      'Installs/Configures notification'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
