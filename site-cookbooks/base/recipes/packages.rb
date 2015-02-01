fec39f2bb4935e6db71085f0584b5e3d4fd3565d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-02-01 11:06:46 +0900

    [base] Install `Ruby` 2.0 for Ubuntu Trusty Tahr.

--- site-cookbooks/base/recipes/packages.rb
+++ site-cookbooks/base/recipes/packages.rb
@@ -44,17 +44,27 @@ end
 # Ruby installation
 case node['platform']
 when 'ubuntu'
-  # common package: ruby
-  package 'ruby' do
-    action :install
-  end
-
-  # install ruby gem
-  if 12.10 <= node['platform_version'].to_f
-    package 'rubygems-integration' do
+  # For Ubuntu 14.04:
+  if 14.04 == node['platform_version'].to_f
+    package 'ruby2.0' do
       action :install
     end
-  elsif node['platform_version'].to_f < 12.10
+
+    # see: http://ayucat.hatenablog.com/entry/2014/07/21/115416
+    %w(erb gem testrb irb rake ri rdoc ruby).each do |cmd|
+      link "/usr/bin/#{cmd}" do
+        to "/usr/bin/#{cmd}2.0"
+
+        not_if { `/usr/bin/ruby -v`.include?('2.0.0') }
+      end
+    end
+
+  # For Ubuntu 12.04:
+  elsif 12.04 == node['platform_version'].to_f
+    package 'ruby' do
+      action :install
+    end
+
     package 'rubygems' do
       action :install
     end
