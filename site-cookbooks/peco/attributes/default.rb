712e48a90af6ecf089af7ac34848624744317cca
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2015-06-28 18:51:46 +0800

    Use the latest `peco` 0.3.2:
    ```
    --- a/site-cookbooks/peco/attributes/default.rb
    +++ b/site-cookbooks/peco/attributes/default.rb
    @@ -1,5 +1,5 @@
     # default values:
    
    -default['peco']['version'] = '0.2.11'
    +default['peco']['version'] = '0.3.2'
     default['peco']['url']     = "https://github.com/peco/peco/releases/download/v#{node['peco']['version']}/peco_linux_amd64.tar.gz"
     default['peco']['prefix']  = '/usr/local'
    ```

--- site-cookbooks/peco/attributes/default.rb
+++ site-cookbooks/peco/attributes/default.rb
@@ -1,5 +1,5 @@
 # default values:
 
-default['peco']['version'] = '0.2.11'
+default['peco']['version'] = '0.3.2'
 default['peco']['url']     = "https://github.com/peco/peco/releases/download/v#{node['peco']['version']}/peco_linux_amd64.tar.gz"
 default['peco']['prefix']  = '/usr/local'
