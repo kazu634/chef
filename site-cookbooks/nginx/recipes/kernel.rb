0caa37a934bd3f37eaa2775282b026784da88f7e
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2016-01-03 15:56:06 +0800

    Increase the # of TCP socket backlogs to 4096.
    - Deploy the configuration file under `/etc/sysctl.d`
    - Add test for deployment

---
+++ site-cookbooks/nginx/recipes/kernel.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: kernel
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# kernel parameters:
+%w( 90-net-core-somaxconn.conf ).each do |conf|
+  cookbook_file "/etc/sysctl.d/#{conf}" do
+    owner 'root'
+    group 'root'
+
+    mode 0644
+  end
+end
