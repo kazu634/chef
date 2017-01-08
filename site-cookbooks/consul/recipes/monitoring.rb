64affeaa6785aab092481daed9d45c580db21898
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-01-08 22:22:21 +0800

    Deploy `consul` monitoring config files:
    - Add the `consul` configuration for checking disk capacity:
    - Add the `consul` configuration for checking load average:
    - Add the `consul` configuration file for checking SSH:
    - Add the `consul` configuration for checking swap:
    
    ```
    --- a/site-cookbooks/consul/recipes/monitoring.rb
    +++ b/site-cookbooks/consul/recipes/monitoring.rb
    @@ -13,3 +13,14 @@ package 'nagios-plugins' do
    
       options '--no-install-recommends'
     end
    +
    +# Deploy `consul` monitoring config file:
    +%w( disk load ssh swap ).each do |target|
    +  cookbook_file "/etc/consul.d/check-#{target}.json" do
    +    owner 'root'
    +    group 'root'
    +    mode 0o644
    +
    +    notifies :run, 'service[consul]'
    +  end
    +end
    ```

--- site-cookbooks/consul/recipes/monitoring.rb
+++ site-cookbooks/consul/recipes/monitoring.rb
@@ -13,3 +13,14 @@ package 'nagios-plugins' do
 
   options '--no-install-recommends'
 end
+
+# Deploy `consul` monitoring config file:
+%w(disk load ssh swap).each do |target|
+  cookbook_file "/etc/consul.d/check-#{target}.json" do
+    owner 'root'
+    group 'root'
+    mode 0o644
+
+    notifies :reload, 'service[consul]'
+  end
+end
