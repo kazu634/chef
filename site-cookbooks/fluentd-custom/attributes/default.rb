4bcda403be98ef06460fa6cdbf229dce6f9c3d44
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-04-23 17:26:35 +0800

    Delete no longer used attributes.

--- site-cookbooks/fluentd-custom/attributes/default.rb
+++ site-cookbooks/fluentd-custom/attributes/default.rb
@@ -5,5 +5,4 @@ default['td_agent']['forward']      = false
 default['td_agent']['base']         = true
 
 # domain configuration
-default_unless['common']['domain']  = 'com'
-default['fluentd-custom']['domain'] = node['common']['domain']
+default['fluentd-custom']['role'] = 'primary'
