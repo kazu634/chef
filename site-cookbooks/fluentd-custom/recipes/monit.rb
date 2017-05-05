2656df1bb05141c5b37783471d6a4f44852d0a5f
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-05-05 22:52:39 +0800

    Update the config file for monitoring `/var/log/monit.log`.

--- site-cookbooks/fluentd-custom/recipes/monit.rb
+++ site-cookbooks/fluentd-custom/recipes/monit.rb
@@ -7,17 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Install prerequisite gems
-%w(fluent-plugin-rewrite-tag-filter
-   fluent-plugin-forest
-   fluent-plugin-grep
-   fluent-plugin-record-reformer
-).each do |pkg|
-  td_agent_gem pkg do
-    action :upgrade
-  end
-end
-
 # deploy the configuration file for monitoring /var/log/monit.log
 cookbook_file '/etc/td-agent/conf.d/forwarder_monit.conf' do
   source 'forwarder_monit.conf'
@@ -25,22 +14,7 @@ cookbook_file '/etc/td-agent/conf.d/forwarder_monit.conf' do
   owner 'root'
   group 'root'
 
-  mode 0644
-
-  notifies :restart, 'service[td-agent]'
-end
-
-# deploy the configuration file for processing /var/log/monit.log
-cookbook_file '/etc/td-agent/conf.d/processor_monit.conf' do
-  source 'processor_monit.conf'
-
-  owner 'root'
-  group 'root'
-
-  mode 0644
-
-  # if the node is the fluentd manager:
-  only_if { node['td_agent']['forward'] }
+  mode 0o644
 
   notifies :restart, 'service[td-agent]'
 end
