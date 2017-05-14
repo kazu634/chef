5e2334c21f2f321781409f058245e7a27bfa5566
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-05-14 22:31:50 +0800

    Integrate `monit` with `systemd`.

--- site-cookbooks/monit/recipes/default.rb
+++ site-cookbooks/monit/recipes/default.rb
@@ -17,7 +17,7 @@ end
 
 cookbook_file '/etc/monit/monitrc' do
   source 'monitrc'
-  mode 0600
+  mode 0o600
   owner 'root'
   group 'root'
 
@@ -28,7 +28,7 @@ case
 when node['platform_version'].to_f < 16.04
   template '/etc/init/monit.conf' do
     source 'monit.conf.erb'
-    mode 0644
+    mode 0o644
     owner 'root'
     group 'root'
 
@@ -50,5 +50,35 @@ when node['platform_version'].to_f < 16.04
   end
 
 when node['platform_version'].to_f < 16.04
-  # do nothing
+  cookbook_file '/etc/default/monit' do
+    source 'monit'
+    mode 0o644
+    owner 'root'
+    group 'root'
+
+    notifies :run, 'script[systemctl daemon-reload]'
+  end
+
+  cookbook_file '/lib/systemd/system/monit.service' do
+    source 'monit.service'
+    mode 0o644
+    owner 'root'
+    group 'root'
+
+    notifies :run, 'script[systemctl daemon-reload]'
+  end
+
+  script 'systemctl daemon-reload' do
+    interpreter 'bash'
+
+    user 'root'
+    group 'root'
+
+    code <<-EOH
+    /etc/init.d/monit stop && systemctl daemon-reload
+    systemctl enable monit && systemctl start monit
+    EOH
+
+    action :nothing
+  end
 end
