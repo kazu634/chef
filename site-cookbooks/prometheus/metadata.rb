2ba22b48ab759d806d7b2137f351b729168aa358
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-06-10 19:20:49 +0800

    Create `Prometheus` cookbook.

---
+++ site-cookbooks/prometheus/metadata.rb
@@ -0,0 +1,9 @@
+name             'prometheus'
+maintainer       'Kazuhiro Musashi'
+maintainer_email 'simoom634@yahoo.co.jp'
+license          'All rights reserved'
+description      'Installs/Configures prometheus'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'supervisor'
