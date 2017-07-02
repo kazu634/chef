acd859e3d9fafbdee74dabae6c1f5d99b8d34d90
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2017-07-02 22:43:22 +0800

    Add tests.

--- site-cookbooks/consul/test/integration/default/serverspec/setup_spec.rb
+++ site-cookbooks/consul/test/integration/default/serverspec/setup_spec.rb
@@ -19,3 +19,10 @@ describe file('/etc/monit/conf.d/consul.conf') do
   it { should be_grouped_into 'root' }
   it { should be_mode 644 }
 end
+
+describe iptables do
+  it { should have_rule('-A FWR -p tcp -m tcp --dport 8300 -j ACCEPT') }
+  it { should have_rule('-A FWR -p tcp -m tcp --dport 8301 -j ACCEPT') }
+  it { should have_rule('-A FWR -p udp -m udp --dport 8301 -j ACCEPT') }
+  it { should have_rule('-A FWR -p tcp -m tcp --dport 8500 -j ACCEPT') }
+end
